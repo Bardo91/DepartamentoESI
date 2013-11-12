f0ac73ea8cdf33db1d29a7e82ff525a957b53616
Bardo91 <19Bardo91@gmail.com>
2013-11-12 11:23:28 +0100

    -- SingleCameraGroundTracking --
    --> Added files related to ObjectMatching

---
+++ SingleCameraGroundTracking/SingleCameraGroundTracking/colorclustersegmentation/ObjectMatching.h
@@ -0,0 +1,74 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//	Color Cluster Segmentation Stereo Tracking
+//
+//		Author: Pablo Ramón Soria
+//		Date: 2013/11/12
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Object Matching
+
+#ifndef OBJECTMATCHING_H_
+#define OBJECTMATCHING_H_
+
+#include "SegmentedObject.h"
+
+#include <vector>
+
+namespace ccss {
+
+	double energyDistance(cv::Point2d obj1, cv::Point2d obj2){
+		return sqrt(pow((obj1.x + obj2.x)/2, 2) + pow((obj1.y + obj2.y)/2,2));
+	}
+	
+	struct trackedObject{
+		cv::Point2d mPos;
+		double mSize;
+		bool flagUpdate;
+
+		trackedObject();
+		trackedObject(cv::Point2d _pos, double _size){
+			mPos = _pos;
+			mSize = _size;
+			flagUpdate = false;
+		}
+	};
+
+	class ObjectMatching{
+	public:
+		void compareAndUpdate(const std::vector<SegmentedObject>& _objs);
+
+		double objectDistance(const trackedObject& _obj1, const trackedObject& _obj2);
+
+		void getCurrentObject(trackedObject& _obj, const int _color) const; 
+
+		void setFlags(bool flag);
+
+	public:
+		ObjectMatching();
+		~ObjectMatching();
+	private:
+		trackedObject *mCurrentObjs;
+
+		double mVelocity;
+
+	};
+
+
+	class StereoObjectMatching{
+	public:
+		void compareAndUpdate(const std::vector<SegmentedObject>& _objs1, const std::vector<SegmentedObject>& _objs2);
+
+		void getCurrentObjects(trackedObject& _obj1, trackedObject& _obj2, const int _color) const;
+
+		void setFlags(bool flag);
+	private:
+		ccss::ObjectMatching mObjMatching1;
+		ccss::ObjectMatching mObjMatching2;
+
+
+
+	};
+
+} // namespace ccss
+
+#endif // OBJECTMATCHING_H_
