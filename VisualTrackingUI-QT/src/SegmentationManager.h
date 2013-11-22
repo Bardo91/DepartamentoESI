421f6d6b7596301253dc328e99191cee69f0579c
Pablo <pablo@pablo-ubuntu.(none)>
2013-11-22 17:03:12 +0100

    ------- VISUAL TRACKING UI QT --------
    --> Change source folder
    --> Created MAKEFILE
    
    ---< Can compile because cant find QT libraries

---
+++ VisualTrackingUI-QT/src/SegmentationManager.h
@@ -0,0 +1,44 @@
+////////////////////////////////////////////////////////////////////////////////
+//	Visual Tracking UI
+//		Author: Pablo Ramón Soria
+//		Date: 2013/11/19
+////////////////////////////////////////////////////////////////////////////////
+// This class manage to collect all info setted at the UI and prepare it to be sent to the algorithm's thread
+
+
+#ifndef _VISUAL_TRACKING_UI_SEGMENTATION_MANAGER_H_
+#define _VISUAL_TRACKING_UI_SEGMENTATION_MANAGER_H_
+
+#include <Types/SimpleObject.h>
+
+#include <opencv/cv.h>
+#include <vector>
+
+namespace vision{
+
+	enum eSegmentationAlgorithms {ColorClustering, RandomClustering};
+
+	class SegmentationManager{
+	public: // Constructors and deconstructors
+		SegmentationManager();
+		~SegmentationManager();
+
+	public: // Config manager
+		void setAlgorithm(eSegmentationAlgorithms _algorithm);
+		eSegmentationAlgorithms getAlgorithm();
+
+	public: // Manager external actions.
+		int applyAlgorithm(cv::Mat _frame, unsigned int threshold, std::vector<SimpleObject> _objects);
+		int applyAlgorithm(cv::Mat _frame1, cv::Mat _frame2, unsigned int threshold, std::vector<SimpleObject> _objects1, std::vector<SimpleObject> _objects2);
+
+	private:
+		eSegmentationAlgorithms eAlgorithm;
+		unsigned int threshold;
+
+
+
+	};
+} //namespace vision
+
+
+#endif //_VISUAL_TRACKING_UI_SEGMENTATION_MANAGER_H_
---
+++ VisualTrackingUI-QT/src/VisualTracking/VisualTracking.pro
@@ -0,0 +1,17 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-11-22T12:20:50
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = VisualTracking
+TEMPLATE = app
+
+
+SOURCES +=
+
+HEADERS  += mainwindow.h
